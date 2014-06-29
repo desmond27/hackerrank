d73e33bb52651a2b785519db6207e422a9db541b
desmond27 <desmond27@users.noreply.github.com>
2014-06-29 16:55:18 +0530

    Create ServiceLane.java
    Solution for the Service Lane problem on Hackerrank.com.
    
    Submission results :
    
    # 0 0.2s : Success
    
    # 1 0.19s : Success
    
    # 2 0.22s : Success
    
    # 3 0.42s : Success
    
    # 4 0.47s : Success
    
    # 5 0.6s : Success
    
    # 6 0.55s : Success
    
    # 7 0.4s : Success
    
    # 8 0.75s : Success
    
    # 9 0.76s : Success
    
    # 10 0.76s : Success
    
    # 11 0.78s : Success

---
+++ ServiceLane.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static int maxVeh(int[] lanes, int ent, int ext){
+		int max=3;
+		for(int i = ent; i <= ext; i++){
+			if(lanes[i]<max) max=lanes[i];
+		}
+		return max;
+	}
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		int T = s.nextInt();
+		int[] lanes = new int[N];
+		for(int i=0; i<N; i++)
+			lanes[i] = s.nextInt();
+		for(int i=0; i<T; i++){
+			int ent = s.nextInt();
+			int ext = s.nextInt();
+			System.out.println(maxVeh(lanes, ent, ext));
+		}
+	}
+}
